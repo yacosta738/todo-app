ac989e70ec47e9e2b0f2296511ce2ea5b7c79bed
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2022-01-04 22:21:39 -0300

    Completed tab

--- tests/e2e/integration/basic_spec.ts
+++ tests/e2e/integration/basic_spec.ts
@@ -1,37 +1,56 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Homepage', () => {
-  it('Shows correct text', () => {
-    cy.visit('/')
+    it('Shows correct text', () => {
+        cy.visit('/')
 
-    cy.contains('h1', '#todo')
-  })
+        cy.contains('h1', '#todo')
+    })
 })
 
 describe('Todos', () => {
-  it('Add a todo', () => {
-    cy.visit('/')
+    it('Add a todo', () => {
+        cy.visit('/')
 
-    cy.get('input[name=todo]').type('Buy milk')
-    cy.get('button[type=submit]').click()
+        cy.get('input[name=todo]').type('Buy milk')
+        cy.get('button[type=submit]').click()
 
-    cy.contains('li', 'Buy milk')
-  })
+        cy.contains('li', 'Buy milk')
+    })
 
-  it('Check a todo', () => {
-    cy.visit('/')
+    it('Check a todo', () => {
+        cy.visit('/')
 
-    cy.get('input[name=todo]').type('Buy beer')
-    cy.get('button[type=submit]').click()
+        cy.get('input[name=todo]').type('Buy beer')
+        cy.get('button[type=submit]').click()
 
-    cy.contains('li', 'Buy beer')
+        cy.contains('li', 'Buy beer')
 
-    // Select the checkbox with label "Buy beer"
-    cy.get('input[type=checkbox]').check()
+        // Select the checkbox with label "Buy beer"
+        cy.get('input[type=checkbox]').check()
 
-    // Check that the todo is checked
-    cy.get('input[type=checkbox]').should('be.checked')
-  })
+        // Check that the todo is checked
+        cy.get('input[type=checkbox]').should('be.checked')
+    })
 })
 
+describe('Completed Tab', () => {
+    it('Shows completed todos', () => {
+        cy.visit('/')
+
+        cy.get('input[name=todo]').type('Buy milk')
+        cy.get('button[type=submit]').click()
+
+        cy.get('input[name=todo]').type('Buy beer')
+        cy.get('button[type=submit]').click()
+
+        cy.get('input[type=checkbox]').check()
+
+        cy.get('li[name=Completed]').click()
+
+        cy.contains('li', 'Buy milk').should('be.visible')
+
+        cy.contains('li', 'Buy beer').should('not.be.visible')
+    })
+})
 
